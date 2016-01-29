19cf67746321cbe2d559cede6637bedd95dc3159
hbeham <horst@beham.biz>
2016-01-29 20:26:19 +0100

    - more flexible feeder startup/restart script
    - added script to merge servers in DB

--- cronjobs/feeder.sh
+++ cronjobs/feeder.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 cd ~xonstat/xonstat/feeder
-killall node 2>/dev/null
-nohup node feeder.node.js -c cfg.json >>feeder.log 2>&1 &
+list=$1
+if test -z "$list"; then
+  list="1"
+fi
+
+for inst in $list
+do
+  echo starting feeder $inst
+  pid=`ps -ef | grep "node feeder.node.js -c cfg$inst.json" | grep -v "grep" | cut -c 10-14`
+  if [ -n "$pid" ]; then
+    kill $pid
+    sleep 1
+  fi
+  nohup node feeder.node.js -c cfg$inst.json >>feeder$inst.log 2>&1 &
+done
