65363619d5709d05667f6f00690d19ceceab49d1
hbeham <horst@beham.biz>
2016-03-28 12:31:47 +0200

    fixed startup of paster to use correct config file

--- cronjobs/paster.sh
+++ cronjobs/paster.sh
@@ -2,4 +2,4 @@
 cd ~xonstat/xonstat
 killall paster 2>/dev/null
 export PYTHONPATH=/home/xonstat/xonstat/lib/python
-~xonstat/xonstat/cronjobs/autorestart.sh nohup paster serve development.ini >>paster.log 2>&1 &
+~xonstat/xonstat/cronjobs/autorestart.sh nohup paster serve qlstats.ini >>paster.log 2>&1 &
