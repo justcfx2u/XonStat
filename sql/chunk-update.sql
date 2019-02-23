97c6e93b9a2bcf91633c23312614a937eacf8fd8
hbeham <horst@beham.biz>
2019-02-23 21:46:02 +0100

    - added exception handlers
    - reorganized zmq connection handling

---
+++ sql/chunk-update.sql
@@ -0,0 +1,17 @@
+﻿do $$
+declare 
+  start integer := 0;
+  step integer := 1000;
+  maxid integer;
+begin
+  select max(game_id) into maxid from games;
+  loop
+    raise notice 'ids starting with %', start;
+    update games set match_id=m.match_id from tmp_game_id m where m.min=games.game_id and games.game_id between start and start+step;
+    start := start+step;
+    exit when start > maxid;    
+  end loop;
+end
+$$ language plpgsql;
+
+
